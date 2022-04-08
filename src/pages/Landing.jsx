2f88009d3178d5232c141f9854d7d7dd8b5147a5
majoalba20 <mjalba458@gmail.com>
2022-04-08 12:40:01 -0500

    Reforming landing

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import yo from '../../img/Yov2.png'
+import tierra from '../../img/tierra.png'
 import Typewriter from 'typewriter-effect';
 import { FaArrowRight } from "react-icons/fa";
 import '../styles/style.css'
@@ -8,8 +8,8 @@ import ParticleBackground from '../components/ParticleBackground'
 const Landing = () => {
 
     const [state] = useState({
-        title: "Hi, I'm ",
-        titleTwo: "María José Alba"
+        title: "Hi, I'm María",
+        titleTwo: "José Alba"
     })
 
     return (
@@ -17,11 +17,11 @@ const Landing = () => {
             <ParticleBackground/>
             <section className='p-32 pb-0 flex'>
                 <div className='lg:w-1/2 w-full justify-center pt-12'>
-                    <h2 className='text-7xl font-af text-[#89694E]'>
+                    <h2 className='text-7xl font-fredoka'>
                         <div className='title'>{state.title}</div>
                         <div className='titleTwo'>{state.titleTwo}</div>
                     </h2>
-                    <div className='text-3xl py-6 font-rubik text-[#89694E]'>
+                    <div className='text-3xl py-6 font-rubik text-[#ffff]'>
                         <Typewriter
                             options={{
                                 autoStart: true,
@@ -34,13 +34,13 @@ const Landing = () => {
                         />
                     </div>
                     <div className='lg:pt-3'>
-                        <button className='btnP z-30 relative shadow-2xl w-52 flex justify-around p-5 font-rubik text-white font-bold rounded-full bg-[#BE3034]'>
+                        <button className='btnP z-30 relative shadow-2xl w-52 flex justify-around p-5 font-rubik text-white font-bold rounded-full bg-[#7557d2]'>
                         VIEW PROJECTS<FaArrowRight color='white' fontSize='1.5em'/>
                         </button>
                     </div>
                 </div>
                 <div className='lg:w-1/2 w-0 flex justify-center'>
-                    <img className='imgYo z-30 hidden lg:block lg:w-9/12 md:w-60' src={yo} alt="" />
+                    <img className='imgYo z-30 hidden lg:block lg:w-9/12 md:w-60' src={tierra} alt="" />
                 </div>
             </section>
         </>
