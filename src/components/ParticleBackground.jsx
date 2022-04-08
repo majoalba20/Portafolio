97ad56e2a605fb66e70e9e203cbfc31d1653f208
majoalba20 <mjalba458@gmail.com>
2022-04-08 12:35:22 -0500

    Changing background style

--- src/components/ParticleBackground.jsx
+++ src/components/ParticleBackground.jsx
@@ -4,116 +4,116 @@ import '../styles/particles.css'
 const ParticleBackground = () => {
     return (
         <Particles
-        options={{
-            particles: {
-                number: {
-                    value: 10,
-                    density: {
-                        enable: true,
-                        value_area: 1736.1522131994905
-                    }
-                },
-                color: {
-                    value: "#ea6f2d"
-                },
-                shape: {
-                    type: "circle",
-                    stroke: {
-                        width: 0,
-                        color: "#000"
-                    },
-                    polygon: {
-                        nb_sides: 12
-                    },
-                    image: {
-                        src: "img/github.svg",
-                        width: 100,
-                        height: 100
-                    }
-                },
-                opacity: {
-                    value: 0.5,
-                    random: true,
-                    anim: {
-                        enable: false,
-                        speed: 1,
-                        opacity_min: 0.1,
-                        sync: false
-                    }
-                },
-                size: {
-                    value: 160,
-                    random: false,
-                    anim: {
-                        enable: true,
-                        speed: 10,
-                        size_min: 40,
-                        sync: false
-                    }
-                },
-                line_linked: {
-                    enable: false,
-                    distance: 819.8939057285982,
-                    color: "#ffffff",
-                    opacity: 1,
-                    width: 2
-                },
-                move: {
-                    enable: true,
-                    speed: 8,
-                    direction: "none",
-                    random: true,
-                    straight: false,
-                    out_mode: "out",
-                    bounce: false,
-                    attract: {
-                        enable: false,
-                        rotateX: 600,
-                        rotateY: 1200
-                    }
-                }
-            },
-            interactivity: {
-                detect_on: "canvas",
-                events: {
-                    onhover: {
-                        enable: false,
-                        mode: "grab"
-                    },
-                    onclick: {
-                        enable: false,
-                        mode: "push"
-                    },
-                    resize: true
-                },
-                modes: {
-                    grab: {
-                        distance: 400,
-                        line_linked: {
-                            opacity: 1
+            options={{
+                "particles": {
+                    "number": {
+                        "value": 298,
+                        "density": {
+                            "enable": true,
+                            "value_area": 800
                         }
                     },
-                    bubble: {
-                        distance: 400,
-                        size: 40,
-                        duration: 2,
-                        opacity: 8,
-                        speed: 3
+                    "color": {
+                        "value": "#ffffff"
                     },
-                    repulse: {
-                        distance: 200,
-                        duration: 0.4
+                    "shape": {
+                        "type": "circle",
+                        "stroke": {
+                            "width": 0,
+                            "color": "#000000"
+                        },
+                        "polygon": {
+                            "nb_sides": 5
+                        },
+                        "image": {
+                            "src": "img/github.svg",
+                            "width": 100,
+                            "height": 100
+                        }
                     },
-                    push: {
-                        particles_nb: 4
+                    "opacity": {
+                        "value": 1,
+                        "random": true,
+                        "anim": {
+                            "enable": true,
+                            "speed": 1,
+                            "opacity_min": 0,
+                            "sync": false
+                        }
                     },
-                    remove: {
-                        particles_nb: 2
+                    "size": {
+                        "value": 3,
+                        "random": true,
+                        "anim": {
+                            "enable": false,
+                            "speed": 4,
+                            "size_min": 0.3,
+                            "sync": false
+                        }
+                    },
+                    "line_linked": {
+                        "enable": false,
+                        "distance": 150,
+                        "color": "#ffffff",
+                        "opacity": 0.4,
+                        "width": 1
+                    },
+                    "move": {
+                        "enable": true,
+                        "speed": 1,
+                        "direction": "none",
+                        "random": true,
+                        "straight": false,
+                        "out_mode": "out",
+                        "bounce": false,
+                        "attract": {
+                            "enable": false,
+                            "rotateX": 600,
+                            "rotateY": 600
+                        }
                     }
-                }
-            },
-            retina_detect: true
-        }} 
+                },
+                "interactivity": {
+                    "detect_on": "canvas",
+                    "events": {
+                        "onhover": {
+                            "enable": true,
+                            "mode": "bubble"
+                        },
+                        "onclick": {
+                            "enable": true,
+                            "mode": "repulse"
+                        },
+                        "resize": true
+                    },
+                    "modes": {
+                        "grab": {
+                            "distance": 400,
+                            "line_linked": {
+                                "opacity": 1
+                            }
+                        },
+                        "bubble": {
+                            "distance": 250,
+                            "size": 0,
+                            "duration": 2,
+                            "opacity": 0,
+                            "speed": 3
+                        },
+                        "repulse": {
+                            "distance": 400,
+                            "duration": 0.4
+                        },
+                        "push": {
+                            "particles_nb": 4
+                        },
+                        "remove": {
+                            "particles_nb": 2
+                        }
+                    }
+                },
+                "retina_detect": true
+            }}
         />
     );
 }
