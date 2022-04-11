ae1fc8ea3dbb7bc0530621d5abd669803a9c3972
majoalba20 <mjalba458@gmail.com>
2022-04-10 21:44:04 -0500

    ending navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,22 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom';
 import cometa from '../../img/cometa.png'
+import '../styles/style.css'
 
 const Navbar = () => {
 
     return (
-        <nav className='opacity-0 sm:opacity-100 flex w-full h-20 fixed top-0 items-center'>
+        <nav className='z-50 opacity-0 sm:opacity-100 flex w-full h-20 fixed top-0 items-center'>
             <div className='w-1/4 flex justify-center'>
                 <img className='w-16' src={cometa} alt="" />
             </div>
             <div className='w-3/4'>
-                <ul className='flex font-fredoka text-xl justify-around cursor-pointer'>
-                    <li><NavLink className={({isActive}) => (isActive ? 'text-[#b150a4]' : 'text-white hover:text-[#b150a4]')} to={'/'}>Home</NavLink></li>
-                    <li><NavLink to={'/about'}>About Me</NavLink></li>
-                    <li><NavLink to={'/portafolio'}>Portafolio</NavLink></li>
-                    <li><NavLink to={'/contact'}>Contact Me</NavLink></li>
-                </ul>
+                <div className='flex font-fredoka text-xl justify-around'>
+                    <NavLink className={({ isActive }) => (isActive ? 'active' : 'noActive')} to={'/'}>Home</NavLink>
+                    <NavLink className={({ isActive }) => (isActive ? 'active' : 'noActive')} to={'/about'}>About Me</NavLink>
+                    <NavLink className={({ isActive }) => (isActive ? 'active' : 'noActive')} to={'/portafolio'}>Portafolio</NavLink>
+                    <NavLink className={({ isActive }) => (isActive ? 'active' : 'noActive')} to={'/contact'}>Contact Me</NavLink>
+                </div>
             </div>
         </nav>
     )
