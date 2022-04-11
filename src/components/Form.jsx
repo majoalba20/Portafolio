e7da6dbc5a22f8bae7a1d423131a2ae779734409
majoalba20 <mjalba458@gmail.com>
2022-04-11 18:57:43 -0500

    Adding form funcionality

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,19 +1,32 @@
 import React from 'react'
+import emailjs from 'emailjs-com';
 
 const Form = () => {
-    return (
-        <form className='p-3 flex flex-col justify-start' action="">
-            <label className='font-rubik' htmlFor="">Correo remitente</label>
-            <input className='rounded-lg bg-slate-100' type="email" />
-            <label className='font-rubik' htmlFor="">Asunto</label>
-            <input className='rounded-lg bg-slate-100' type="text" />
-            <label className='font-rubik' htmlFor="">Message</label>
-            <textarea className='rounded-lg bg-slate-100' name="" id="" rows="6"></textarea>
-            <button className='m-auto mt-3 z-30 relative w-32 p-2 font-rubik text-white font-bold rounded-full bg-[#7557d2]' type='submit'>
-                Enviar
-            </button>
-        </form>
-    )
-}
 
-export default Form
+    const sendEmail = (e) => {
+        e.preventDefault();
+        emailjs.sendForm('service_r2uvdnc', 'template_9i473tc', e.target, 'zZp7mNfs8kxSN2DEt')
+            .then((result) => {
+                console.log(result.text);
+            }, (error) => {
+                console.log(error.text);
+            });
+        e.target.reset();
+    };
+
+        return (
+            <form onSubmit={sendEmail} className='p-3 flex flex-col justify-start'>
+                <label className='font-rubik' htmlFor="">Correo remitente</label>
+                <input className='rounded-lg bg-slate-100' type="email" name='from_name' />
+                <label className='font-rubik' htmlFor="">Asunto</label>
+                <input className='rounded-lg bg-slate-100' type="text" name='subject' />
+                <label className='font-rubik' htmlFor="">Message</label>
+                <textarea className='rounded-lg bg-slate-100' name="message" rows="6"></textarea>
+                <button className='m-auto mt-3 z-30 relative w-32 p-2 font-rubik text-white font-bold rounded-full bg-[#7557d2]' type='submit'>
+                    Enviar
+                </button>
+            </form>
+        )
+    }
+
+    export default Form
