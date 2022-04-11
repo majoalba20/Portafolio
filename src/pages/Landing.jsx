f084510c89c35a6f34fefd74ec28be0c36e4e76e
majoalba20 <mjalba458@gmail.com>
2022-04-10 21:43:33 -0500

    Fixing responsive

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -17,13 +17,13 @@ const Landing = () => {
         <>
             <Navbar/>
             <ParticleBackground/>
-            <section className='p-32 pb-0 flex'>
+            <section className='md:p-32 md:pb-0 p-16 flex'>
                 <div className='lg:w-1/2 w-full justify-center pt-12'>
                     <h2 className='text-7xl font-fredoka'>
                         <div className='title'>{state.title}</div>
                         <div className='titleTwo'>{state.titleTwo}</div>
                     </h2>
-                    <div className='text-3xl py-6 font-rubik text-[#ffff]'>
+                    <div className='text-3xl py-6 md:h-28 h-28 font-rubik text-[#ffff]'>
                         <Typewriter
                             options={{
                                 autoStart: true,
@@ -42,7 +42,7 @@ const Landing = () => {
                     </div>
                 </div>
                 <div className='lg:w-1/2 w-0 flex justify-center'>
-                    <img className='imgYo z-30 hidden lg:block lg:w-9/12 md:w-60' src={tierra} alt="" />
+                    <img className='tierra z-30 hidden lg:block lg:w-9/12' src={tierra} alt="" />
                 </div>
             </section>
         </>
