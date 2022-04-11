805880952b79256938123c4199365b8056ba8f4c
majoalba20 <mjalba458@gmail.com>
2022-04-10 21:43:08 -0500

    Adding card in portafolio

--- src/pages/Portafolio.jsx
+++ src/pages/Portafolio.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import Cards from "../components/Cards"
+import Navbar from '../components/Navbar'
+import ParticleBackground from '../components/ParticleBackground'
 
 const Portafolio = () => {
     return (
-        <div className="p-24 flex gap-8 flex-wrap">
-            <Cards/>
-        </div>
+        <>
+            <Navbar/>
+            <ParticleBackground/>
+            <div className='flex w-full justify-center'>
+                <div className='flex flex-wrap gap-8 justify-center items-center pt-36 w-11/12'>
+                    <Cards/>
+                </div>
+            </div>
+        </>
     )
 }
 
