733bf31a0b0f424022f3c63394955b78e3abf69b
majoalba20 <mjalba458@gmail.com>
2022-04-18 09:40:56 -0500

    Adding about me text after limit date

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -14,9 +14,15 @@ const About = () => {
                     <img className='yo z-30  lg:w-9/12 md:w-60' src={yo} alt="" />
                 </div>
                 <div className='text lg:w-1/2 w-full font-rubik text-white justify-center pt-12'>
-                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maxime expedita ducimus, odit rem aspernatur voluptatum laboriosam quaerat architecto voluptatibus ipsam sed autem pariatur dolorem fugit impedit dignissimos esse non similique!
-                    Magnam, exercitationem. In tenetur explicabo dignissimos mollitia at rerum sequi eius corrupti nobis, nulla fuga eligendi quidem, officia itaque repellat nemo animi autem excepturi optio. Incidunt qui optio saepe doloribus.
-                    Nihil, veritatis! Dolores corrupti voluptatibus ipsam molestiae nam aspernatur esse, sed qui. Maiores aspernatur obcaecati adipisci pariatur eligendi, eos deserunt aliquam, officiis, qui accusantium odit amet similique magni atque iure!
+                Soy María José Alba, tengo 22 años, soy ingeniería electrónica 
+                en formación, apasionada por la tecnología y el desarrollo de 
+                software, busco contribuir en este sector con mis habilidades y 
+                enriquecer mis conocimientos técnicos. He desarrollado 
+                aplicaciones del lado del cliente y del lado del servidor con 
+                tecnologías como react js, php, MongoDB y laravel llegando a 
+                desempeñarme como desarrolladora web full stack. Con alto 
+                sentido de responsabilidad, trabajo en equipo y comunicación 
+                asertiva.
                 </div>
             </section>
             <NavbarFloatting/>
